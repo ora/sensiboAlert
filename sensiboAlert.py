5be2c0bdf7d6067cde6ff36208ff01fa588abe3e
Oleg <olegr@live.com>
2022-03-19 21:12:16 -0400

    root - Mon Feb 21 18:44:50 EST 2022

--- sensiboAlert.py
+++ sensiboAlert.py
@@ -11,30 +11,35 @@ sensibo_apiKey = "SENSIBO APIKEY"
 sg_apiKey = "SENDGRID APIKEY"
 sendTo = "EMAIL ADDRESS"
 
-resp = requests.get("https://home.sensibo.com/api/v2/users/me/pods?fields=connectionStatus{isAlive},measurements&apiKey=%s" %(sensibo_apiKey))
+resp = requests.get(
+    "https://home.sensibo.com/api/v2/users/me/pods?fields=connectionStatus{isAlive},measurements&apiKey=%s" % (sensibo_apiKey))
 
 if not resp:
-    print("%s: Exiting" %resp.status_code)
+    print("%s: Exiting" % resp.status_code)
     exit()
 
 data = resp.json()
 
-temperature = (int(round((9 * data["result"][0]["measurements"]["temperature"]) / 5 + 32)))
+temperature = (
+    int(round((9 * data["result"][0]["measurements"]["temperature"]) / 5 + 32)))
 
 print("Response:", resp.status_code)
 print("Status:", data["result"][0]["connectionStatus"]["isAlive"])
 print("Temperature:", temperature)
 
+
 def sendMail(alert_body):
-	message = Mail(from_email='sensibo_alert@no_reply.com', to_emails=sendTo, subject='Sensibo Alert', html_content=alert_body)
-	sg = SendGridAPIClient(sg_apiKey)
-	response = sg.send(message)
-	print(response.status_code)
-	print(response.body)
-	print(response.headers)
+    message = Mail(from_email='sensibo_alert@no_reply.com',
+                   to_emails=sendTo, subject='Sensibo Alert', html_content=alert_body)
+    sg = SendGridAPIClient(sg_apiKey)
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
 
 if ((data["result"][0]["connectionStatus"]["isAlive"]) == 0):
-	sendMail("<strong>Sensibo is offline<strong>")
+    sendMail("<strong>Sensibo is offline<strong>")
 
 if (temperature < 56):
-	sendMail("<strong>Sensibo temperature is " + str(temperature) + "<strong>")
+    sendMail("<strong>Sensibo temperature is " + str(temperature) + "<strong>")
